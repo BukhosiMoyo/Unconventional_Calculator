1b9b8ecd04f6fc2c1d45216295605a356f87c7b5
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-12-11 01:23:43 +0200

    Creating the logic of the calculator app

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('This works!');
+const  deafultResult = 0;
+let currentResult = deafultResult;
+
+currentResult = (currentResult + 10) * 3 / 2 - 1;
+
+let calculationDescription = `(${deafultResult} + 10) * 3 / 2 - 1`;
+
+outputResult(currentResult, calculationDescription);
