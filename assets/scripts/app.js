a3e3371280ab3a162e1150bf43e912d8f5df2b81
Bukhosi Moyo <bukhosizimcode@gmail.com>
2020-12-11 01:41:54 +0200

    Adding the add fuction to the code

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,6 +1,14 @@
 const  deafultResult = 0;
 let currentResult = deafultResult;
 
+function add(num1, num2){
+    const result = num1 + num2;
+    alert(`This result is ${result}`)
+}
+
+add(20, 7);
+add(90, 67);
+
 currentResult = (currentResult + 10) * 3 / 2 - 1;
 
 let calculationDescription = `(${deafultResult} + 10) * 3 / 2 - 1`;
